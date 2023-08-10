e964d0c0d092bc285c67ace8f27060b45bf9a745
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-08-10 20:48:10 +0500

    jallod Api

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = [
+        {'name':'Dennis', 'age':28},
+        {'name':'Jhon', 'age':20},
+        {'name':'Dennis', 'age':28},
+        {'name':'Jhon', 'age':20},
+    ]
+    return Response(person)
