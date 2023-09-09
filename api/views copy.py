0375a980977233865a2ae86225bb085f03e1c80c
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-09-09 10:52:06 +0500

    apis

--- api/views copy.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def getData(request):
-    person = [
-        {'question':'olmalar', 'answer': 'apples'},
-    ]
-    return Response(person)
