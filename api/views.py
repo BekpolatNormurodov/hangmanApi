f3107151fa5f303bfb51245c100097721a1ab72b
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-09-21 17:49:46 +0500

    API

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def getData(request):
     person = [
+        {'question':'oq', 'answer': 'white'},
         {'question':'qizil', 'answer': 'red'},
         {'question':'sariq', 'answer': 'yellow'},
         {'question':'kok', 'answer': 'blue'},
