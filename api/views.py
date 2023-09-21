be28b5bd8f161bdd7cd8cf6feb96adfea7b4e870
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-09-21 16:08:00 +0500

    API

--- api/views.py
+++ api/views.py
@@ -7,16 +7,6 @@ def getData(request):
         {'question':'kitob', 'answer': 'book'},
         {'question':'olma', 'answer': 'apple'},
         {'question':'ism', 'answer': 'name'},
-        {'question':'yosh', 'answer': 'age'},
-        {'question':'salom', 'answer': 'hello'},
-        {'question':'yordam', 'answer': 'help'},
-        {'question':'inson', 'answer': 'person'},
-        {'question':'rasm', 'answer': 'picture'},
-        {'question':'ruchka', 'answer': 'per'},
-        {'question':'qizil', 'answer': 'red'},
-        {'question':'sariq', 'answer': 'yellow'},
-        {'question':'kok', 'answer': 'blue'},
-        {'question':'yashil', 'answer': 'green'},
-        {'question':'qora', 'answer': 'black'},
+
     ]
     return Response(person)
