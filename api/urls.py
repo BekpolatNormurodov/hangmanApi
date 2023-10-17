40920a8bd02760e74dc52498fbca46f026c74c31
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-10-17 16:35:11 +0500

    add

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HangmanApiView
+from .views import HangmanApiView, HangmanApiCreate, HangmanApiUpdate
 
 urlpatterns = [
     path('',HangmanApiView.as_view()),
+    path('add/',HangmanApiCreate.as_view()),
+    path('<int:pk>',HangmanApiUpdate.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,15 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from api.models import Hangman
 from .serializers import Hangmanserializer
 
 class HangmanApiView(ListAPIView):
+    queryset = Hangman.objects.all()
+    serializer_class = Hangmanserializer
+
+class HangmanApiCreate(ListCreateAPIView):
+    queryset = Hangman.objects.all()
+    serializer_class = Hangmanserializer
+
+class HangmanApiUpdate(RetrieveUpdateDestroyAPIView):
     queryset = Hangman.objects.all()
     serializer_class = Hangmanserializer
